2f134e387c97a31d91a3181634f57c27937d9724
Lucas Rafaell do Nascimento Santos <lucas.rafaell@outlook.com>
2023-06-29 10:50:53 -0300

    feat(error) - basic error type implementation
    - creation of `Error` and `CommonError` classes;
    - writing unit test scenarios for errors;

---
+++ src/Core/Errors/Error.cs
@@ -0,0 +1,39 @@
+﻿using Horizon.Returnables.Core.Errors.Enums;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Horizon.Returnables.Core.Errors;
+
+[DataContract]
+public record Error
+{
+    [DataMember, JsonPropertyName("code")]
+    public string Code { get; }
+
+    [DataMember, JsonPropertyName("type")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public ErrorType Type { get; }
+
+    [DataMember, JsonPropertyName("message")]
+    public string Message { get; }
+
+    /// <summary>
+    /// Initialize the error with code and message description
+    /// </summary>
+    /// <exception cref="ArgumentNullException"></exception>
+    public Error(string code, string message, ErrorType type = ErrorType.BusinessError)
+    {
+        Type = type;
+        Code = string.IsNullOrWhiteSpace(code) ? throw new ArgumentNullException(nameof(code)) : code;
+        Message = string.IsNullOrWhiteSpace(message) ? throw new ArgumentNullException(nameof(message)) : message;
+    }
+
+    public static Error Create(string code, string message, ErrorType type = ErrorType.BusinessError)
+        => new(code, message, type);
+
+    public override string ToString()
+        => $"Code: {Code}\nMessage: {Message}\nType: {Type}";
+
+    public static implicit operator string(Error error)
+        => error.ToString();
+}
---
+++ tests/Core.Tests/Errors/ErrorUnitTests/ToStringUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Horizon.Returnables.Core.Errors;
+using Xunit;
+
+namespace Horizon.Returnables.Core.Tests.Errors.ErrorUnitTests;
+
+public sealed class ToStringUnitTests
+{
+    [Fact]
+    [Trait(nameof(Error), nameof(Error.ToString))]
+    public void Given_An_Error_A_String_Containing_Its_Code_And_Message_Should_Be_Returned()
+    {
+        // arrange
+        var error = Error.Create("SOME_CODE", "SOME_MESSAGE");
+        var expected = "Code: SOME_CODE\nMessage: SOME_MESSAGE\nType: BusinessError";
+
+        // act
+        var content = error.ToString();
+
+        // assert
+        content.Should().NotBeNullOrEmpty();
+
+        content.Should().Be(expected);
+    }
+}
